d0d12b836e112607a2ddce501151f0409b909352
oiwn <alex@imscraping.ninja>
2022-11-27 21:41:16 +0700

    first good looking version

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::char;
 // Rust implementation of classic Matrix screensaver
 use std::collections::HashMap;
 use std::io::{stdout, Stdout, Write};
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use crossterm::{
     cursor, event, execute,
@@ -32,13 +32,14 @@ static CHARACTERS: Lazy<Vec<char>> = Lazy::new(|| {
     v
 });
 
-static MAX_WORMS: u16 = 10;
+static INITIAL_WORMS: u16 = 60;
+static MAX_WORMS: u16 = 40;
 
 #[derive(Clone)]
 struct VerticalWorm {
     chars: Vec<char>,
-    hx: u16,
-    hy: u16,
+    fx: f32,
+    fy: f32,
     max_length: u8,
     finish: bool,
     speed: u8,
@@ -48,33 +49,48 @@ struct VerticalWorm {
 impl VerticalWorm {
     fn new(w: u16, _h: u16) -> Self {
         let mut rng = rand::thread_rng();
-        let max_length = rng.gen_range(4..10);
         let mut chars: Vec<char> = vec![];
         chars.push(CHARACTERS.choose(&mut rng).unwrap().clone());
 
         Self {
             chars,
-            hx: rng.gen_range(0..w),
-            hy: 0,
-            max_length,
-            speed: 9,
+            fx: rng.gen_range(0..w) as f32,
+            fy: 0.0,
+            max_length: rng.gen_range(4..10),
+            speed: rng.gen_range(2..20),
             finish: false,
             rng,
         }
     }
-    fn update(&mut self, w: u16, h: u16) {
-        // change character
-        if self.chars.len() < self.max_length as usize {
-            self.chars
-                .insert(0, CHARACTERS.choose(&mut self.rng).unwrap().clone());
+
+    fn to_points(&self) -> (u16, u16) {
+        let x = self.fx.round() as u16;
+        let y = self.fy.round() as u16;
+        (x, y)
+    }
+
+    fn reset(&mut self, w: u16, _h: u16) -> Result<()> {
+        self.chars
+            .insert(0, CHARACTERS.choose(&mut self.rng).unwrap().clone());
+        self.chars.truncate(self.max_length as usize);
+        self.fy = 0.0;
+        self.fx = self.rng.gen_range(0..w) as f32;
+        self.speed = self.rng.gen_range(2..20);
+        self.max_length = self.rng.gen_range(4..10);
+        Ok(())
+    }
+
+    fn update(&mut self, w: u16, h: u16, dt: Duration) -> Result<()> {
+        self.chars
+            .insert(0, CHARACTERS.choose(&mut self.rng).unwrap().clone());
+        self.chars.truncate(self.max_length as usize);
+        self.fy = self.fy + ((self.speed as f32 * dt.as_millis() as f32) / 1000.0);
+
+        if self.fy.round() as u16 >= h {
+            self.reset(w, h)?;
         }
-        // shift one line down
-        self.hy += 1;
-        if self.hy >= h {
-            self.hy = 0;
-            self.hx = self.rng.gen_range(0..w);
-            self.finish = true;
-        }
+
+        Ok(())
     }
 }
 
@@ -88,7 +104,7 @@ struct Matrix {
 impl Matrix {
     fn new(width: u16, heigth: u16) -> Self {
         let mut worms: Vec<VerticalWorm> = vec![];
-        for _ in 1..MAX_WORMS {
+        for _ in 1..INITIAL_WORMS {
             worms.push(VerticalWorm::new(width as u16, heigth as u16));
         }
         Self {
@@ -100,15 +116,19 @@ impl Matrix {
 
     fn draw(&self, stdout: &mut Stdout) -> Result<()> {
         for worm in self.worms.iter() {
-            for (pos, wc) in worm.chars.iter().enumerate() {
-                if worm.hy < pos as u16 {
-                    continue;
-                }
-                let hy = worm.hy - pos as u16;
-                if (hy > 1) && (hy < self.screen_height) {
-                    stdout
-                        .queue(cursor::MoveTo(worm.hx, hy))?
-                        .queue(style::PrintStyledContent(wc.green()))?;
+            let (x, y) = worm.to_points();
+            for (pos, char) in worm.chars.iter().enumerate() {
+                if (y as i16 - pos as i16) > 0 {
+                    stdout.queue(cursor::MoveTo(x, y - pos as u16))?;
+
+                    match pos {
+                        0 => stdout.queue(style::PrintStyledContent(char.white().bold()))?,
+                        1 => stdout.queue(style::PrintStyledContent(char.white()))?,
+                        2 => stdout.queue(style::PrintStyledContent(char.green()))?,
+                        3 => stdout.queue(style::PrintStyledContent(char.dark_green()))?,
+                        10..=20 => stdout.queue(style::PrintStyledContent(char.black()))?,
+                        _ => stdout.queue(style::PrintStyledContent(char.dark_grey()))?,
+                    };
                 }
             }
         }
@@ -117,7 +137,11 @@ impl Matrix {
 
     fn update(&mut self) -> Result<()> {
         for worm in self.worms.iter_mut() {
-            worm.update(self.screen_width, self.screen_height);
+            worm.update(
+                self.screen_width,
+                self.screen_height,
+                Duration::from_millis(50),
+            )?;
         }
         Ok(())
     }
@@ -156,10 +180,11 @@ fn main() -> Result<()> {
         is_running = Matrix::process_input()?;
         matrix.draw(&mut stdout)?;
         stdout.flush()?;
+        // std::thread::sleep(Duration::from_millis(16));
         matrix.update()?;
     }
 
-    execute!(stdout, terminal::LeaveAlternateScreen)?;
+    execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen)?;
     terminal::disable_raw_mode()?;
     Ok(())
 }
